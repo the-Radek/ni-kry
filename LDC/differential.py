cc05110bd8a9e6e87e436ca9b1175acb34491c9e
Radek Večerník <vecerra1@fit.cvut.cz>
2024-11-18 01:23:10 +0100

    SPN cryptoanalysis

--- LDC/differential.py
+++ LDC/differential.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 
-__author__      = "Tomas Zvara, Tomas Fornusek, Robert Lorencz, Jiri Bucek"
+__author__ = "Tomas Zvara, Tomas Fornusek, Robert Lorencz, Jiri Bucek"
 
 import argparse
-from spn import encrypt,substitution_dec
+from spn import encrypt, substitution_dec
+
+HIT_TABLE_SIZE = 32
+
 
 def extract_key_bytes(key_mask):
     res = []
-    for byte in range(0,4):
-        if(((key_mask >> byte) % 2) == 1):
-            res.append(byte*4)
+    for byte in range(0, 4):
+        if ((key_mask >> byte) % 2) == 1:
+            res.append(byte * 4)
     return res
+
 
 def generate_keys(k_bytes):
     n = len(k_bytes)
@@ -18,73 +22,94 @@ def generate_keys(k_bytes):
     for i in range(16**n):
         bins = [0xF & i, (0xF0 & i) >> 4, (0xF00 & i) >> 8, (0xF000 & i) >> 12]
         acc = 0
-        for j,b in enumerate(k_bytes):
+        for j, b in enumerate(k_bytes):
             acc |= bins[j] << b
         res.append(acc)
     return res
 
+
 def hamming_weight(bits):
-#Brian Kernighan's algorithm
+    # Brian Kernighan's algorithm
     count = 0
     while bits:
         bits = bits & (bits - 1)
         count += 1
     return count
 
+
 def reverse_last_round(block, key):
     return substitution_dec(block ^ key)
 
-def differential_cryptoanalysis(enc_key, size, deltaP, expected_deltaU4, key_mask, prob):
+
+def differential_cryptoanalysis(
+    enc_key, size, deltaP, expected_deltaU4, key_mask, prob
+):
     key_bytes = extract_key_bytes(key_mask)
     if not key_bytes:
         raise ValueError("At least one bit has to be set in key_mask.")
     key_bytes_len = len(key_bytes)
     possible_keys = generate_keys(key_bytes)
 
-    pt_1 = [i+1 for i in range(size)]
-    pt_2 = [i^deltaP for i in pt_1]
-    ct_1 = [encrypt(i,enc_key) for i in pt_1]
-    ct_2 = [encrypt(i,enc_key) for i in pt_2]
+    pt_1 = [i + 1 for i in range(size)]
+    pt_2 = [i ^ deltaP for i in pt_1]
+    ct_1 = [encrypt(i, enc_key) for i in pt_1]
+    ct_2 = [encrypt(i, enc_key) for i in pt_2]
 
-    hit_table = [0] * (2**(16))
-    max_hit_table = [{'key':0,'prob': 0,'Q':1} for i in range(20)] #key, prob, Q
+    hit_table = [0] * (2 ** (16))
+    max_hit_table = [
+        {"key": 0, "prob": 0, "Q": 1} for i in range(HIT_TABLE_SIZE)
+    ]  # key, prob, Q
 
     count = 0
     for n in range(size):
         for key in possible_keys:
-            U1 = reverse_last_round(ct_1[n],key)
-            U2 = reverse_last_round(ct_2[n],key)
+            U1 = reverse_last_round(ct_1[n], key)
+            U2 = reverse_last_round(ct_2[n], key)
             deltaU = U1 ^ U2
             if deltaU == expected_deltaU4:
-                hit_table[key]+=1
-        count+=1
+                hit_table[key] += 1
+        count += 1
 
     for key in possible_keys:
-        hit_table[key] = abs(hit_table[key]/count)
+        hit_table[key] = abs(hit_table[key] / count)
         Q = abs(prob - hit_table[key])
-        #save the maximum
-        if Q < max_hit_table[-1]['Q']:
-            max_hit_table[-1]['key'] = key
-            max_hit_table[-1]['prob'] = hit_table[key]
-            max_hit_table[-1]['Q'] = Q
-            max_hit_table = sorted(max_hit_table, key=lambda x: x['Q'], reverse=False)
+        # save the maximum
+        if Q < max_hit_table[-1]["Q"]:
+            max_hit_table[-1]["key"] = key
+            max_hit_table[-1]["prob"] = hit_table[key]
+            max_hit_table[-1]["Q"] = Q
+            max_hit_table = sorted(max_hit_table, key=lambda x: x["Q"], reverse=False)
 
     return max_hit_table
 
+
 def main():
     # %%%%%% DEFAULT ARGS %%%%%% #
-    enc_key=0x1345 #key for encryption - normally, this is unknown, and not used later
-    size = 5000 #number of plaintext block generated
-    deltaP = 0b0000101100000000 #what bits differ at P
-    expected_deltaU4 = 0b0000011000000110 #what bits must differ at U4
-    key_mask = 0b0101 #Sbox mask
-    prob = 27/1024
+    enc_key = 0x1345  # normally, this is unknown, and not used later
+    size = 5000  # number of plaintext block generated
+    deltaP = 0b0000101100000000  # what bits differ at P
+    expected_deltaU4 = 0b0000011000000110  # what bits must differ at U4
+    key_mask = 0b0101  # Sbox mask
+    prob = 27 / 1024
 
-    max_hits = differential_cryptoanalysis(enc_key, size, deltaP, expected_deltaU4, key_mask, prob)
+    # Overwrite args
+    enc_key = 0x1345  # key for encryption
+    size = 100  # number of plaintext block generated
+    deltaP = 0b1011_0000_1011_0000  # what bits differ at P
+    expected_deltaU4 = 0b0000_1000_0000_1000  # what bits must differ at U4
+    key_mask = 0b0101  # Sbox mask
+    prob = 9 / 256
 
-    print("Top 20 observed probabilities:")
+    max_hits = differential_cryptoanalysis(
+        enc_key, size, deltaP, expected_deltaU4, key_mask, prob
+    )
+
+    max_hits = sorted(max_hits, key=lambda x: x["prob"], reverse=True)
+
+    print(f"Top {HIT_TABLE_SIZE} observed probabilities:")
     for res in max_hits:
         print(f"Key: 0x{res['key']:04X}, prob: {res['prob']:.4f}, Q: {res['Q']:.4f}")
+
 
 if __name__ == "__main__":
     main()
---
+++ LDC/report.md
@@ -0,0 +1,41 @@
+## Linear cryptanalysis
+
+obrázek
+
+| S-box | input | output | bias |
+| ----- | ----- | ------ | ---- |
+| S11   | A     | 4      | -1/4 |
+| S13   | A     | 4      | -1/4 |
+| S22   | A     | 4      | -1/4 |
+| S32   | 4     | 5      | -1/4 |
+
+```
+bias = 2**3 * (-1/4)**4 = 1 / 32 = 0.03125
+P_mask = 1010_0000_1010_0000
+U_mask = 0000_0100_0000_0100
+```
+
+### Výsledky
+
+Analýza byla spuštěna na plaintext/ciphertext s klíčem 0xBEAF a o velikosti 1000, 5000 a 10 000 bloků (viz složka `test/`). Analýza útočila na 2. a 4. bajt klíče. Hledaným výsledkem byl tedy 0x0E0F. Výsledek byl nalzen na vzorku 5000 bloků na 9. místě (seřazeno podle biasu). V případě 10 000 bloků měl správný výsledek největší bias.
+
+## Differential cryptanalysis
+
+obrázek
+
+| S-box | input | output | propability |
+| ----- | ----- | ------ | ----------- |
+| S11   | B     | 2      | 1/2         |
+| S13   | B     | 2      | 1/2         |
+| S23   | A     | 8      | 3/8         |
+| S31   | 2     | 5      | 3/8         |
+
+```
+propability = (1/2)**2 * (3/8)**2 = 9 / 256 = 0.03515625
+deltaP_mask = 1011_0000_1011_0000
+deltaU_mask = 0000_1000_0000_1000
+```
+
+### Výsledky
+
+Klíč byl zvolen znovu 0xBEAF. Analýza útočila na 2. a 4. bajt klíče. Už při pouhých 100 blocích bylo možné nalézt správné řešení 0x0E0F s nejvyší pravděpodobností ze všech klíčů. Byly testovány i jiné klíče, aby se redukovala náhoda ve prospěch analýzy.
