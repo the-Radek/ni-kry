8a4d9cad753d2a6ace1954d4a0c5fd9d542bd658
Radek Večerník <vecerra1@fit.cvut.cz>
2024-12-10 13:58:29 +0100

    DSA collision

--- DSA/clean.py
+++
@@ -1,5 +0,0 @@
-with open("words.txt", "r") as f1, open("clean.txt", "w") as f2:
-    for line in f1:
-        word = line.strip()
-        if len(word) > 4 and word.isalpha() and word.islower() and len(set(word)) > 2:
-            f2.write(word + "\n")
